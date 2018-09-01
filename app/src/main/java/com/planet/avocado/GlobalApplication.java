8944d52c1e87b0294a93412bc268bc0caaa199d9
Jaeyoung <jaeyoung@AL01352718.local>
2018-09-02 03:16:40 +0900

    Add webp logo

--- app/src/main/java/com/planet/avocado/GlobalApplication.java
+++ app/src/main/java/com/planet/avocado/GlobalApplication.java
@@ -2,6 +2,7 @@ package com.planet.avocado;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
 import com.tsengvn.typekit.Typekit;
 
 public class GlobalApplication extends Application {
@@ -13,6 +14,7 @@ public class GlobalApplication extends Application {
                 .addNormal(Typekit.createFromAsset(this, "NanumSquareRoundOTFR.otf"))
                 .addBold(Typekit.createFromAsset(this, "NanumSquareRoundOTFEB.otf"))
                 .addItalic(Typekit.createFromAsset(this, "NanumSquareRoundOTFL.otf"))
-                .addBoldItalic(Typekit.createFromAsset(this, "NanumSquareRoundOTFB.otf"))
+                .addBoldItalic(Typekit.createFromAsset(this, "NanumSquareRoundOTFB.otf"));
+        Fresco.initialize(this);
     }
 }
--- app/src/main/java/com/planet/avocado/ui/activities/LoginActivity.java
+++ app/src/main/java/com/planet/avocado/ui/activities/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.planet.avocado.ui.activities;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.planet.avocado.R;
 import com.planet.avocado.managers.LoginManager;
 import com.planet.avocado.ui.base.BaseActivity;
@@ -21,21 +28,38 @@ public class LoginActivity extends BaseActivity {
     private CompositeDisposable mDisposables = new CompositeDisposable();
     private Button mBtnGoogle;
     private Button mBtnFacebook;
+    private SimpleDraweeView mDraweeLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        initButtons();
+        initViews();
+        initAnimation();
         checkLoginStatus();
     }
 
-    private void initButtons() {
-        Log.d(TAG, "initButtons: ");
+    private void initAnimation() {
+        Log.d(TAG, "initAnimation: ");
+
+        Context ctx = this;
+        Resources resources = ctx.getResources();
+        int resId = R.drawable.avocado;
+        Uri uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + resources.getResourcePackageName(resId) + '/' + resources.getResourceTypeName(resId) + '/' + resources.getResourceEntryName(resId));
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setUri(uri)
+                .setAutoPlayAnimations(true)
+                .build();
+        mDraweeLogo.setController(controller);
+    }
+
+    private void initViews() {
+        Log.d(TAG, "initViews: ");
 
         mBtnGoogle = findViewById(R.id.btn_googleLogin);
         mBtnFacebook = findViewById(R.id.btn_facebookLogin);
+        mDraweeLogo = findViewById(R.id.drawee_logo);
 
         mBtnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
