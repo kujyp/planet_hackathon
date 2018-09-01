699714ff67fe2ee445496293a1da8618c6bc8c0e
namsukim <niceguy0825@naver.com>
2018-09-02 05:03:29 +0900

    그리드 뷰 추가2.

--- app/src/main/java/com/planet/avocado/fragment/SnackFragment.java
+++ app/src/main/java/com/planet/avocado/fragment/SnackFragment.java
@@ -1,5 +1,7 @@
 package com.planet.avocado.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,7 +10,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.planet.avocado.R;
 import com.planet.avocado.adapter.GridViewAdapter;
@@ -26,16 +30,31 @@ public class SnackFragment extends Fragment {
     private GridView gridView;
     private GridViewAdapter gridAdapter;
     private View view;
+    private Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         view = inflater.inflate(R.layout.fragment_snack, container, false);
+        context = container.getContext();
 
         gridView = (GridView) view.findViewById(R.id.gridView);
         gridAdapter = new GridViewAdapter(getContext(), R.layout.grid_item_layout, getData());
         gridView.setAdapter(gridAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+                Toast.makeText(context, item.getTitle(), Toast.LENGTH_SHORT).show();
+                /*Intent intent = new Intent(SnackFragment.this, DetailsActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("image", item.getImage());
+                startActivity(intent);*/
+            }
+        });
 
         return view;
     }
