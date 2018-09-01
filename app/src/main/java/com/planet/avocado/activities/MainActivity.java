425d84ffea96c2a7734c831595dde9477ae3cb27
namsukim <niceguy0825@naver.com>
2018-09-02 00:53:39 +0900

    바텀네이게이션뷰 추가.

--- app/src/main/java/com/planet/avocado/activities/MainActivity.java
+++ app/src/main/java/com/planet/avocado/activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.planet.avocado.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.planet.avocado.R;
 import com.planet.avocado.consts.Consts;
@@ -17,8 +21,11 @@ import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
     private CompositeDisposable mProductDisposable = new CompositeDisposable();
+
+    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_detail).setOnClickListener(v -> {
             gotoDetailActivity("1");
         });
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     private void gotoDetailActivity(String productId) {
@@ -66,4 +77,24 @@ public class MainActivity extends AppCompatActivity {
     private void updateProductList(List<Product> productList) {
         Log.d(TAG, "updateProductList() called with: productList = [" + productList + "]");
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
